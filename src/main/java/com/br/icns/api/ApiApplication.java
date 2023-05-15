9cf819a569c7e13ce41cfee17bef998585e50fd1
Diogo Henrique Barbosa <contato.diogohbarbosa@gmail.com>
2023-05-14 22:00:53 -0300

    Criação para verificação das variaveis de ambientes

--- src/main/java/com/br/icns/api/ApiApplication.java
+++ src/main/java/com/br/icns/api/ApiApplication.java
@@ -1,4 +1,5 @@
 package com.br.icns.api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -6,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println(System.getenv("API_SECRET"));
+		System.out.println(System.getenv("API_ISSUER"));
+		System.out.println(System.getenv("USERBANCO"));
+		System.out.println(System.getenv("PASSWORD"));
+		System.out.println(System.getenv("PORT"));
+		System.out.println(System.getenv("BD"));
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
